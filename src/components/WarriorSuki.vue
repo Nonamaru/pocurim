aa116913bc42afb727420c784efc37075872cc6d
Nonamaru <zykinf@gmail.com>
2023-07-14 16:57:28 +0500

    work

--- src/components/WarriorSuki.vue
+++ src/components/WarriorSuki.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="warrior">
-    <div>{{ name }} {{ role }}</div>
+    <div v-bind:class="{youWarrior: isYou}">
+      {{ role }}
+      {{ name }}
+      {{ isYou }}
+    </div>
     <div>
-      <text @click="$emit('ready', true)">READY?</text>
+      <!-- <text @click="$emit('ready', true)">READY?</text> -->
+      <button @click="isReady = !isReady" :disabled="!isYou">READY?</button>
+      <text v-if="isReady">HE IS READY!</text>
     </div>
   </div>
 </template>
 <script>
 export default{
-  props: ['name', 'role']
+  props: ['name', 'role', 'isYou'],
+  data(){
+    return{
+      isReady: false,
+    }
+  }
 }
 </script>
+<style scoped>
+.warrior{
+  width: 18%;
+  border: 1px solid black;
+}
+.youWarrior{
+  border: 2px solid green;
+}
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -4,11 +4,16 @@
     <div class="header">
       {{ role }}
       {{ whois }}
-      {{ suka }}
+      {{ isReady }}
     </div>
     <div class="hub">
       <div class="isReady">
-        <WarriorSuki @ready="(i) => suka = i" name="Felix" role="Задумчивый шаман" />
+        <!-- <WarriorSuki @ready="(i) => isReady = i" :name="whois" :role="role" /> -->
+        <WarriorSuki :name="whois" :role="role" :isYou=true />
+        <WarriorSuki name="Anton" role="Танчащий палладин" :isYou=false />
+        <WarriorSuki name="Danil" role="Длинный убийца" :isYou=false />
+        <WarriorSuki name="Ruslan" role="Таинственный маг" :isYou=false />
+        <WarriorSuki name="Sanya" role="Агро воин" :isYou=false />
       </div>
     </div>
   </div>
@@ -24,7 +29,39 @@ export default{
     return{
       whois: '',
       role: '',
-      suka: ''
+      isReady: '',
+      characters:[
+        {
+          id: 'a38f9dd3-263f-4f64-9314-b6e171fe5b75',
+          name: 'Felix',
+          title: 'Задумчивый шаман',
+          picture: 'felix',
+        },
+        {
+          id: 'f786b04d-5388-413c-83c9-dcf8c88b885e',
+          name: 'Anton',
+          title: 'Танчащий палладин',
+          picture: 'antoxa',
+        },
+        {
+          id: '07dd2448-4e50-4d77-acb2-29a0ab9aa2d4',
+          name: 'Danil',
+          title: 'Длинный убийца',
+          picture: 'danil',
+        },
+        {
+          id: '4dfd4089-0d29-43ee-acda-7cee22a59bf8',
+          name: 'Ruslan',
+          title: 'Таинственный маг',
+          picture: 'ruslan',
+        },
+        {
+          id: 'a14ae894-32f0-4be5-9d54-d58333e7f3fc',
+          name: 'Sanya',
+          title: 'Агро воин',
+          picture: 'sanya',
+        },
+      ]
     }
   },
   mounted(){
@@ -52,9 +89,10 @@ export default{
   border: 1px solid black;
 }
 .isReady{
-  width: 30%;
+  width: 100%;
   border: 1px solid black;
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
 }
 </style>
